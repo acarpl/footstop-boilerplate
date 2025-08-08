637eaf0d1197411d7b5029e23f091a66fa96eafb
acarpl <rasya.gani@smkn1kotabekasi.sch.id>
2025-08-08 14:40:09 +0700

    styling midtrans

--- footstop-frontend/app/(authenticated)/checkout/page.tsx
+++ footstop-frontend/app/(authenticated)/checkout/page.tsx
@@ -140,8 +140,6 @@ const CheckoutPageContent = () => {
 
   return (
     <div className="max-w-4xl mx-auto p-4 md:p-10 mt-10">
-      <div id="snap-container"></div>
-
       <Typography.Title level={2} className="mb-8">
         Checkout
       </Typography.Title>
@@ -229,6 +227,8 @@ const CheckoutPageContent = () => {
             </div>
           </div>
         </Card>
+
+        <div id="snap-container"></div>
       </div>
     </div>
   );
