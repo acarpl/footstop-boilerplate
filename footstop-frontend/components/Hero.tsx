eccb5aae7bb1840d92f5aa57292ea0495d6b0ee0
Pukakaaay <Briliantamam45@gmail.com>
2025-08-08 11:37:04 +0700

    a

--- footstop-frontend/components/Hero.tsx
+++ footstop-frontend/components/Hero.tsx
@@ -4,7 +4,7 @@ export default function Hero() {
   return (
     <div className="relative w-full h-screen pt-20">
       <Image
-        src="/images/hero-section.png"
+        src="/images/hero-bnr.png"
         alt="Hero banner"
         fill
         style={{ objectFit: "cover" }}
