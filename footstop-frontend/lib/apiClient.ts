ef769cf9bdc9f5718de558c1ec1bc6c0d78452ca
acarpl <rasya.gani@smkn1kotabekasi.sch.id>
2025-08-21 15:21:33 +0700

    fix bug kemarin

--- footstop-frontend/lib/apiClient.ts
+++ footstop-frontend/lib/apiClient.ts
@@ -1,26 +1,8 @@
-import axios, { AxiosHeaders } from "axios";
-import { TokenUtil } from "../utils/token";
+import axios from "axios";
 
 const apiClient = axios.create({
   baseURL: process.env.NEXT_PUBLIC_API_BASE_URL,
   withCredentials: true,
 });
-
-apiClient.interceptors.request.use(
-  (config) => {
-    if (TokenUtil.accessToken) {
-      // Pastikan headers berupa AxiosHeaders
-      if (!config.headers) {
-        config.headers = new AxiosHeaders();
-      }
-      (config.headers as AxiosHeaders).set(
-        "Authorization",
-        `Bearer ${TokenUtil.accessToken}`
-      );
-    }
-    return config;
-  },
-  (error) => Promise.reject(error)
-);
 
 export default apiClient;
